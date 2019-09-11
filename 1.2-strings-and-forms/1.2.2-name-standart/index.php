e2c92fc40a21182221c8f353a43945dffab585c3
Anton Iusupov <an87yu@gmail.com>
2019-09-11 14:16:51 +0300

    string & forms. all tasks done

---
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -0,0 +1,13 @@
+<?php
+    $firstName = mb_ucfirst(mb_strtolower(trim($_POST['firstName'])));
+    $lastName = mb_ucfirst(mb_strtolower(trim($_POST['lastName'])));
+    $middleName = mb_ucfirst(mb_strtolower(trim($_POST['middleName'])));
+    $fullName = $lastName . ' ' . $firstName . ' ' . $middleName;
+    $fio = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) . mb_substr($middleName, 0, 1);
+    $surnameAndInitials = $lastName . ' ' . mb_substr($firstName, 0, 1) . '.' . mb_substr($middleName, 0, 1) . '.';
+    echo $fullName;
+    echo "<br> ${fio}";
+    echo "<br> ${surnameAndInitials}";
+    function mb_ucfirst($string) {
+        return mb_strtoupper(mb_substr($string, 0, 1)) . mb_substr($string, 1);
+    }
--- index.php
+++ index.php
@@ -5,19 +5,33 @@
     <title>bPHP</title>
 </head>
 <body>
-<ul>
-    <li>Блок 1. Базовый синтаксис. PHP в HTML и HTML средствами PHP
-        <ul>
-            <li>Занятие 1.1. Знакомство с PHP
-                <ul>
-                    <li><a href="1.1-intro-and-branching/1.1.0">Задание 1.1.0</a></li>
-                    <li><a href="1.1-intro-and-branching/1.1.1">Задание 1.1.1</a></li>
-                    <li><a href="1.1-intro-and-branching/1.1.2">Задание 1.1.2</a></li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-</ul>
+    <ul>
+        <li>Блок 1. Базовый синтаксис. PHP в HTML и HTML средствами PHP
+            <ul>
+                <li>Занятие 1.1. Знакомство с PHP
+                    <ul>
+                        <li><a href="1.1-intro-and-branching/1.1.0">Задание 1.1.0</a></li>
+                        <li><a href="1.1-intro-and-branching/1.1.1">Задание 1.1.1</a></li>
+                        <li><a href="1.1-intro-and-branching/1.1.2">Задание 1.1.2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <br>
+        <li>Блок 2. Формы и строки
+            <ul>
+                <li>Занятие 1.2.
+                    <ul>
+                        <li><a href="1.2-strings-and-forms/1.2.1-registration">Задание 1.2.1</a></li>
+                        <li><a href="1.2-strings-and-forms/1.2.2-name-standart">Задание 1.2.2</a></li>
+                        <li><a href="1.2-strings-and-forms/1.2.3-additional">Задание 1.2.3</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <br>
+        
+    </ul>
 </body>
 </html>
 
